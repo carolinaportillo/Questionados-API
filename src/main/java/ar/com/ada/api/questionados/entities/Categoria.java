2da8eab47d69c4656adc3f6085844df486d3fc58
carolinaportillo <carolinaa.portillo@hotmail.com>
2021-07-02 18:48:27 -0300

    Se crea el esqueleto del proyecto, con sus correspondientes packages y tres entities base que aun faltan completar.

---
+++ src/main/java/ar/com/ada/api/questionados/entities/Categoria.java
@@ -0,0 +1,10 @@
+package ar.com.ada.api.questionados.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+    
+}
---
+++ src/main/java/ar/com/ada/api/questionados/entities/Pregunta.java
@@ -0,0 +1,10 @@
+package ar.com.ada.api.questionados.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pregunta")
+public class Pregunta {
+    
+}
